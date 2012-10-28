2ebebb85e17217af69ae5f6412bb21748d9c515f
Reinhard Alischer <reinhard@alischer.eu>
2012-10-28 19:52:48 +0100

    updated 3.2

--- Prak3/src/main/java/edu/hm/vss/prak3/ProducerThread.java
+++ Prak3/src/main/java/edu/hm/vss/prak3/ProducerThread.java
@@ -20,25 +20,28 @@ public class ProducerThread extends AbstractWorkerThread{
 		}
 		Data data = new Data(getName(),currentDataCount);
 		synchronized(MONITOR) {
-			if(queue.size() == MAX_ENTRIES) {
-				synchronized(PRODUCER_MONITOR) {
+			while(queue.size() == MAX_ENTRIES) {
+				//synchronized(PRODUCER_MONITOR) {
 					try {
-						PRODUCER_MONITOR.wait();
+						MONITOR.wait();
 					} catch (InterruptedException ex) {
 						//ignored at this point
 					}
-				}
+				//}
 			}
 			queue.add(data);
 			System.out.println(getName()+" added data to queue "+data);
-			if(queue.size() == 1) {
+		//}
+		//synchronized (MONITOR) {
+			if(queue.size() > 0) {
 				//all consumer should sleep -> notify them
-				synchronized(ConsumerThread.CONSUMER_MONITOR) {
-					ConsumerThread.CONSUMER_MONITOR.notifyAll();
-				}
-			}
-			
+				//synchronized(ConsumerThread.CONSUMER_MONITOR) {
+				//	ConsumerThread.CONSUMER_MONITOR.notifyAll();
+				//}
+				MONITOR.notifyAll();
+			}	
 		}
+			
 		
 		//stopWorker();
 	}

9acc5d935603e7eea1262e3c84a02e0693c8a427
Martin Paul <slaytanic666@gmx.net>
2012-11-22 00:34:30 +0100

    works

--- Prak5/src/edu/hm/vss/rmi/Server.java
+++ Prak5/src/edu/hm/vss/rmi/Server.java
@@ -1,42 +1,48 @@
 package edu.hm.vss.rmi;
 
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-import edu.hm.vss.rmi.interfaces.Calculator;
+import edu.hm.vss.rmi.interfaces.ICalculator;
 
-public class Server implements Calculator {
+public class Server implements ICalculator {
 
 	protected Server() {
 		super();
 	}
 
-	public Double add(double a, double b) {
-		return new Double(a + b);
+	@Override
+	public double add(double a, double b) {
+		return a + b;
 	}
 
-	public Double multiply(double a, double b) {
-		return new Double(a * b);
+	@Override
+	public double multiply(double a, double b) {
+		return a * b;
 	}
 
-	public static void main(String args[]) {
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new SecurityManager());
-		}
-		try {
-			String name = "Calc6";
-			Calculator calc = new Server();
-			Calculator stub = (Calculator) UnicastRemoteObject.exportObject(
-					calc, 0);
-			Registry registry = LocateRegistry.getRegistry();
-			registry.rebind(name, stub);
-			System.out.println("Calc bound");
-		} catch (Exception e) {
-			System.err.println("Calc exception:");
-			e.printStackTrace();
-		}
+	@Override
+	public double subtract(double a, double b) throws RemoteException {
+		return a - b;
+	}
 
+	@Override
+	public double divide(double a, double b) throws RemoteException {
+		return a / b;
+	}
+
+	public static void main(String args[]) throws RemoteException {
+		LocateRegistry.createRegistry(1099);
+		Server server = new Server();
+
+		ICalculator calcStub = (ICalculator) UnicastRemoteObject.exportObject(
+				server, 0);
+
+		Registry registry = LocateRegistry.getRegistry();
+		registry.rebind("server", calcStub);
+		System.out.println("server waiting for requests");
 	}
 
 }
---
+++ Prak5/src/edu/hm/vss/rmi/interfaces/ICalculator.java
@@ -0,0 +1,13 @@
+package edu.hm.vss.rmi.interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ICalculator extends Remote{
+
+	public double add(double a, double b) throws RemoteException;
+	public double subtract(double a, double b) throws RemoteException;
+	public double multiply(double a, double b) throws RemoteException;
+	public double divide(double a, double b) throws RemoteException;
+
+}
